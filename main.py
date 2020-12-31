f9f406a58e4c06245be2980d4522c9b30eb83dc2
rounak bastola <spvobl@rounaks-MacBook-Pro.local>
2020-12-31 13:13:39 -0600

    for mac

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import re
 from datetime import datetime, timedelta
 def time(timeString, addTimeString):
     try:
-        timeObj = datetime.strptime(timeString, '%H:%M:%S,%f')
+        timeObj = datetime.strptime("2020 "+ timeString, '%Y %H:%M:%S,%f')
         addSecond, addMilliSecond = addTimeString.split(".")
     except:
         return timeString
     return (timeObj + timedelta(seconds = int(addSecond), milliseconds = int(addMilliSecond) )).strftime("%H:%M:%S,%f")
 
 if __name__ == "__main__":
-    subFileTxt = open('subFile.srt',"r", encoding='utf-8').read()
-    newFile = open('newFile.srt',"w", encoding='utf-8')
-    addTime = "-43.234"
+    subFileTxt = open('subFile.srt',"r").read()
+    newFile = open('newFile.srt',"w")
+    addTime = "-47.200"
     timeArr = re.findall(r'[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}',subFileTxt)
     for x in timeArr:
         subFileTxt = subFileTxt.replace(x,time(x,addTime))
