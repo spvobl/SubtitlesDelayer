c25f0b65b7599431de8abef4f65ac79ccc47e07e
rounak bastola <spvobl@rounaks-MacBook-Pro.local>
2021-01-01 13:34:02 -0600

    made more readable

--- subFrame.py
+++ subFrame.py
@@ -5,27 +5,28 @@ from getSubFile import subDelay
 
 class SubFrame(tk.Frame):
     def print_value(self, val):
-        self.entry3.delete(-1, 'end')
-        self.entry3.insert(0, str(val))
+        self.timeDelayEntry.delete(-1, 'end')
+        self.timeDelayEntry.insert(0, str(val))
 
     def button_click(self):
-        self.scale.set(self.entry3.get())
-        subDelay(self.entry.get(), self.entry2.get(), self.entry3.get())
+        self.scale.set(self.timeDelayEntry.get())
+        subDelay(self.enterFileEntry.get(),
+                 self.saveFileEntry.get(), self.timeDelayEntry.get())
 
     def file_select(self):
-        self.entry.delete(-1, 'end')
+        self.enterFileEntry.delete(-1, 'end')
         self.file = filedialog.askopenfilename(
             initialdir="/Users/spvobl/Downloads/", title="Select Subtitle file", filetypes=(("sub files", "*.sub *.srt *.mpl *.webvtt *.dfxp *.txt"), ("all files", "*.*")))
-        self.entry.insert(0, self.file)
+        self.enterFileEntry.insert(0, self.file)
 
     def save_file(self):
-        self.entry2.delete(-1, 'end')
-        self.file2 = filedialog.asksaveasfilename(
+        self.saveFileEntry.delete(-1, 'end')
+        self.newFile = filedialog.asksaveasfilename(
             initialdir="/Users/spvobl/Downloads/", title="Select Folder")
-        if "srt" in self.file2 or "sub" in self.file2 or "mpl" in self.file2 or "webvtt" in self.file2 or "dfxp" in self.file2 or "txt" in self.file2:
-            self.entry2.insert(0, self.file2)
+        if "srt" in self.newFile or "sub" in self.newFile or "mpl" in self.newFile or "webvtt" in self.newFile or "dfxp" in self.newFile or "txt" in self.newFile:
+            self.saveFileEntry.insert(0, self.newFile)
         else:
-            self.entry2.insert(0, self.file2+".srt")
+            self.saveFileEntry.insert(0, self.newFile+".srt")
 
     def __init__(self, master=None):
         super().__init__(master)
@@ -33,37 +34,37 @@ class SubFrame(tk.Frame):
         self.master.geometry('1000x515')
         self.master.resizable(0, 0)
         self.master.title('Subtitle Delayer')
-        self.label = tk.Label(self.master, text="Subtitle Delayer", font=(
+        self.title = tk.Label(self.master, text="Subtitle Delayer", font=(
             'Default', 40), bg="gray", width=40, pady=15)
-        self.label2 = tk.Label(self.master, text="Enter Subtitle File",
-                               font=('Default', 18, 'bold'))
-        self.label3 = tk.Label(self.master, text="Enter New Name for Subtitle File",
-                               font=('Default', 18, 'bold'))
-        self.label4 = tk.Label(self.master, text="Delay Time",
-                               font=('Default', 18, 'bold'))
+        self.enterSubFileLabel = tk.Label(self.master, text="Enter Subtitle File",
+                                          font=('Default', 18, 'bold'))
+        self.newSubFileLabel = tk.Label(self.master, text="Enter New Name for Subtitle File",
+                                        font=('Default', 18, 'bold'))
+        self.delayTimeLabel = tk.Label(self.master, text="Delay Time",
+                                       font=('Default', 18, 'bold'))
         self.scale = tk.Scale(self.master, orient=tk.HORIZONTAL,
                               resolution=0.001, length=950, from_=-250, to=250, fg="black", command=self.print_value)  # -600, #600
-        self.button = tk.Button(self.master, text="Create Subtitle File",
-                                width=20, height=3, bg="red", activebackground="red", command=self.button_click, font=('default', 16, 'bold'))
-        self.button2 = tk.Button(
+        self.createSubButton = tk.Button(self.master, text="Create Subtitle File",
+                                         width=20, height=3, bg="red", activebackground="red", command=self.button_click, font=('default', 16, 'bold'))
+        self.selectFileButton = tk.Button(
             self.master, text="Select File", command=self.file_select)
-        self.button3 = tk.Button(
+        self.saveFileButton = tk.Button(
             self.master, text="Save As", command=self.save_file)
-        self.entry = tk.Entry(self.master, width=70)
-        self.entry2 = tk.Entry(self.master, width=70)
-        self.entry3 = tk.Entry(self.master, width=10)
-        self.entry3.insert(0, "0.000")
-        self.label.pack()
-        self.label2.pack(pady=(20, 5))
-        self.entry.pack()
-        self.button2.pack(pady=(5, 0))
-        self.label3.pack(pady=(20, 5))
-        self.entry2.pack()
-        self.button3.pack(pady=(5, 0))
-        self.label4.pack(pady=(20, 0))
+        self.enterFileEntry = tk.Entry(self.master, width=70)
+        self.saveFileEntry = tk.Entry(self.master, width=70)
+        self.timeDelayEntry = tk.Entry(self.master, width=10)
+        self.timeDelayEntry.insert(0, "0.000")
+        self.title.pack()
+        self.enterSubFileLabel.pack(pady=(20, 5))
+        self.enterFileEntry.pack()
+        self.selectFileButton.pack(pady=(5, 0))
+        self.newSubFileLabel.pack(pady=(20, 5))
+        self.saveFileEntry.pack()
+        self.saveFileButton.pack(pady=(5, 0))
+        self.delayTimeLabel.pack(pady=(20, 0))
         self.scale.pack(anchor=tk.CENTER)
-        self.entry3.pack(pady=10)
-        self.button.pack()
+        self.timeDelayEntry.pack(pady=10)
+        self.createSubButton.pack()
 
 
 if __name__ == "__main__":
